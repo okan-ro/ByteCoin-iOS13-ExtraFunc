e3e8f5d85f7e5e6e48f3b5428aebeb8a420a264f
Okan Ozdemir <okan.r.ozdemir@gmail.com>
2022-08-26 23:44:20 +0300

    REST and CoinAPI manager file

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,46 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCurreny(coinManager: CoinManager, ratio: CoinModel)
+}
+
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/"
+    let apiKey = "596AB0A5-2D63-4436-9AAC-91B397561A09"
+    var delegate: CoinManagerDelegate?
+//    let coinArray = ["BTC", "ETH", "LTC", "BNB", "SXP"]
+    let currencyArray = [["BTC", "ETH", "LTC", "BNB", "SXP"],["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]]
 
     
+    func fetchRequest(coinName: String, currencyType: String) {
+        let url = URL(string: "\(baseURL)\(coinName)/\(currencyType)?&apikey=\(apiKey)")!
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil {
+                print(error!)
+            }
+            if let safeData = data {
+                if let ratioData = parseJSON(with: safeData) {
+                    delegate?.didUpdateCurreny(coinManager: self, ratio: ratioData)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func parseJSON(with data: Data) -> CoinModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let ratio = decodedData.rate
+            let ratioData = CoinModel(rate: ratio)
+            return ratioData
+        }
+        catch {
+            print(error)
+            return nil
+        }
+    }
 }
